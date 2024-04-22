0c851b38c407aeab2c20708a5278db6be0714f29
pavan_samanth1 <48581685+Pavan-Samanth@users.noreply.github.com>
2024-04-22 21:38:32 +0530

    Update models.py

--- Project/models.py
+++ Project/models.py
@@ -1,19 +1,19 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence, Float,PrimaryKeyConstraint, ForeignKey,Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship, backref
-from sqlalchemy.sql import *
+# from sqlalchemy import create_engine, Column, Integer, String, Sequence, Float,PrimaryKeyConstraint, ForeignKey,Text
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker, relationship, backref
+# from sqlalchemy.sql import *
 
-Database_url = "mysql://resume:n3tBanking@resumes.cvuksgsuaytw.us-east-1.rds.amazonaws.com:3306/resumes"
-engine = create_engine(Database_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# Database_url = "mysql://resume:n3tBanking@resumes.cvuksgsuaytw.us-east-1.rds.amazonaws.com:3306/resumes"
+# engine = create_engine(Database_url)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "User"
-    id = Column(Integer,primary_key=True)
-    username = Column(String)
-    email = Column(String)
-    password = Column(Text)
+# class User(Base):
+#     __tablename__ = "User"
+#     id = Column(Integer,primary_key=True)
+#     username = Column(String)
+#     email = Column(String)
+#     password = Column(Text)
 
-User.__table__.create(bind=engine, checkfirst=True)
+# User.__table__.create(bind=engine, checkfirst=True)
 
