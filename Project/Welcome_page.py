8adc5e31b552f65f0d70529fbee7fdc9c37c3c27
pavan_samanth1 <48581685+Pavan-Samanth@users.noreply.github.com>
2024-04-22 21:33:20 +0530

    Update Welcome_page.py

--- Project/Welcome_page.py
+++ Project/Welcome_page.py
@@ -14,8 +14,6 @@ html_str = f"""<html><body>
                 <p> In today's industry its difficult to go through each resume and select that particular candidates who are associated to a Job Description .</p>
                 <p> So we designed this app, Where you will give the input of JD file and a set of resumes and it will return the top 5 resumes whose score matches to the given Job Description. </p>
                 <p> Click on the left arrow to register, login and then upload the files of Job Description and multiple resume files.</p>
-                <p> If you hadn't registered {'<a href="https://resume-screening-6exf2w3vbii.streamlit.app/register_page" target="_self">click here!</a>'}</p>
-                <p> If you are already a registered user {'<a href="https://resume-screening-6exf2w3vbii.streamlit.app/login_page" target="_self">click here!</a>'}</p>
                 </html></body>"""
 
 st.markdown(html_str, unsafe_allow_html=True)
